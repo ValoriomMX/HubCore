def203ac94263552f31445e5d12ebd23fd79d90b
Miquel Angel <hulkrojo2@gmail.com>
2021-03-18 13:03:20 +0100

    Added PortalAPI to one class

---
+++ src/main/java/es/hulk/addons/queue/PortalAPI.java
@@ -0,0 +1,32 @@
+package es.hulk.addons.queue;
+
+import me.joeleoli.portal.shared.queue.Queue;
+import org.bukkit.entity.Player;
+
+public class PortalAPI {
+
+    public boolean inQueue(Player p) {
+        Queue queue = Queue.getByPlayer(p.getUniqueId());
+        if (queue != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getQueueIn(Player p) {
+        Queue queue = Queue.getByPlayer(p.getUniqueId());
+        return queue.getName();
+    }
+
+    public int getPosition(Player p) {
+        Queue queue = Queue.getByPlayer(p.getUniqueId());
+        return queue.getPosition(p.getUniqueId());
+    }
+
+
+    public int getInQueue(String queue) {
+        Queue queue1 = Queue.getByName(queue);
+        return queue1.getPlayers().size();
+    }
+}
