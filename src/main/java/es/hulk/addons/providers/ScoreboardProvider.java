696d4d64e4d9405c7cdbe947473f1f452830fb4f
Miquel Angel <hulkrojo2@gmail.com>
2021-03-19 18:00:00 +0100

    Fixed the fucking scoreboard

--- src/main/java/es/hulk/addons/providers/ScoreboardProvider.java
+++ src/main/java/es/hulk/addons/providers/ScoreboardProvider.java
@@ -4,6 +4,7 @@ import es.hulk.addons.HubAddons;
 import es.hulk.addons.utils.Utils;
 import io.github.thatkawaiisam.assemble.AssembleAdapter;
 import me.clip.placeholderapi.PlaceholderAPI;
+import me.joeleoli.portal.shared.queue.Queue;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -20,19 +21,21 @@ public class ScoreboardProvider implements AssembleAdapter {
     public List<String> getLines(final Player player) {
         List<String> toReturn = new ArrayList<>();
 
-        if (HubAddons.getQueue().inQueue(player)) {
-            HubAddons.getInstance().getConfig().getStringList("SCOREBOARD.QUEUED").forEach(line -> {
+        Queue queue = Queue.getByPlayer(player.getUniqueId());
+        if (queue != null) {
+            HubAddons.getInstance().getScoreboardConfig().getStringList("SCOREBOARD.QUEUED").forEach(line -> {
                 line = Utils.color(line);
                 line = PlaceholderAPI.setPlaceholders(player, line);
 
                 toReturn.add(line.
-                        replaceAll("%queue_position", String.valueOf(HubAddons.getQueue().getPosition(player))).
-                        replaceAll("%queue_name%", String.valueOf(HubAddons.getQueue().getQueueIn(player))).
-                        replaceAll("%queue_total%", String.valueOf(HubAddons.getQueue().getInQueue(HubAddons.getQueue().getQueueIn(player)))));
+                        replaceAll("%queue_position%", String.valueOf(queue.getPosition(player.getUniqueId()))).
+                        replaceAll("%queue_name%", String.valueOf(queue.getName())).
+                        replaceAll("%queue_total%", String.valueOf(queue.getPlayers().size())));
             });
         } else {
             HubAddons.getInstance().getScoreboardConfig().getStringList("SCOREBOARD.NORMAL").forEach(line -> toReturn.add(Utils.color(PlaceholderAPI.setPlaceholders(player, line))));
         }
+
         return toReturn;
     }
 }
